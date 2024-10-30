41399566f8c44687202e611d2dad30007e3eb708
Sujit Kumar Libi <sujit_libi@hotmail.com>
2024-10-30 18:04:58 +0545

    startup create page updated unexpected bug fixed

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,20 +1,21 @@
-import {auth} from "@/auth";
-import {redirect} from "next/navigation";
-
 import StartupForm from "@/components/StartupForm";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
     const session = await auth();
 
-    if(!session) redirect("/");
+    if (!session) redirect("/");
 
     return (
-       <>
-           <section className="pink_container !min-h-[230px]">
-               <h1 className="heading">Submit Your Startup Pitch</h1>
-           </section>
-           <StartupForm />
-       </>
-    )
-}
-export default Page
+        <>
+            <section className="pink_container !min-h-[230px]">
+                <h1 className="heading">Submit Your Startup</h1>
+            </section>
+
+            <StartupForm />
+        </>
+    );
+};
+
+export default Page;
